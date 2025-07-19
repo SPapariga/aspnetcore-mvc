7998a0d10965c8e0ea8976d464e4c87175510bf8
Your Name <your.email@example.com>
2025-07-19 03:56:41 +0300

    Initial commit, with project setup and models

---
+++ Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace Restaurant.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public int? Stock { get; set; }
+        public string CategoryId { get; set; } 
+        public Category? Category { get; set; }
+        public ICollection<OrderItem>? OrderItems { get; set; }
+        public ICollection<ProductIngredient>? ProductIngredient { get; set; }
+    }
+}
