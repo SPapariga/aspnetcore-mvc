7ae249d76e1a115fc79608cd664152d1b386ec6e
Your Name <your.email@example.com>
2025-07-19 20:19:35 +0300

    Create IRepository, Repository, Ingredients Controller & Index View

---
+++ Models/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Build.Locator;
+
+namespace Restaurant.Models
+{
+    public interface IRepository<T> where T:class
+    {
+
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id, QueryOptions<T> options);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+
+    }
+}
