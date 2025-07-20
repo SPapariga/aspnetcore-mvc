8245ac63af29f2608797223d5918cf5a9f063fba
Your Name <your.email@example.com>
2025-07-21 00:20:31 +0300

    Details View

--- Controllers/IngredientController.cs
+++ Controllers/IngredientController.cs
@@ -7,13 +7,45 @@ namespace Restaurant.Controllers
     public class IngredientController : Controller
     {
         private Repository<Ingredient> ingredients;
-        public IngredientController(ApplicationDbContext context) 
+        private Repository<Ingredient> _ingredientRepository;
+
+
+        //public IngredientController(ApplicationDbContext context) 
+        //{
+        //    ingredients = new Repository<Ingredient>(context);
+        //}
+        public IngredientController(ApplicationDbContext context)
         {
             ingredients = new Repository<Ingredient>(context);
+            _ingredientRepository = new Repository<Ingredient>(context);
         }
+
         public async Task<IActionResult> Index()
         {
             return View(await ingredients.GetAllAsync());
+        }
+        //public async Task<IActionResult> Details(int id) 
+        //{
+
+
+        //    return View(await ingredients.GetByIdAsync(id, new QueryOptions<Ingredient>() {Includes="ProductIngredients, Product"}));
+        //}
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var options = new QueryOptions<Ingredient>
+            {
+                Includes = "ProductIngredients,ProductIngredients.Product"
+            };
+
+            var ingredient = await _ingredientRepository.GetByIdAsync(id, options);
+
+            if (ingredient == null)
+            {
+                return NotFound();
+            }
+
+            return View(ingredient);
         }
     }
 }
